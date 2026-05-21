1d121158345a8a02824a188fbc1ddf36c856446d
Adam <1sayedmurtaza@gmail.com>
2026-05-21 16:20:40 +0200

    Portfolio updated

--- backend/portfolio_backend/urls.py
+++ backend/portfolio_backend/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='portfolio/index.html'), name='homepage'),
+    path('', TemplateView.as_view(template_name='index.html'), name='homepage'),
     path('api/', include('portfolio.urls')),
 ]
